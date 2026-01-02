fe3b050922f49caccd92dd9558b91d40c53348fe
Daivashree2Bhosekar <sweetybhosekar9011@gmail.com>
2026-01-02 17:22:57 +0530

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,27 @@
+Captain = "XYz"     # String = Enclosed in "" or''
+b = 42              # Integer = Whole number
+c = 4.2             # Float = Decimal number
+d = True            # Boolean = True or False
+print(Captain, b, c, d)
+
+# Variable names should start with a letter or underscore,
+# and can contain letters, numbers, and underscores.
+
+# Examples of valid variable names:
+my_variable = 10
+_variable2 = 20
+variable_3 = 30
+# Examples of invalid variable names (uncommenting these will cause errors):
+# 2variable = 40      # Starts with a number
+# my-variable = 50    # Contains a hyphen
+# my variable = 60    # Contains a space  my_variable = 70
+# Variable names are case-sensitive:
+
+Name = "Daksh"
+print(Name)
+type(c) # Function to check the data type of a variable
+print(type(c))  # Output: <class 'str'>
+
+
+sub = input("Enter your subject: ")
+print("Your subject is:", sub)
